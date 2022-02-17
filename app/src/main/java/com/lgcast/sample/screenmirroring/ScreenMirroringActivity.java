b23fd6e75c11fc868816b9df8b4488a85766c113
lgcast-dev <lgcast-dev@lge.com>
2022-02-17 13:37:53 +0900

    Removed slash comments
    request from commit review

--- app/src/main/java/com/lgcast/sample/screenmirroring/ScreenMirroringActivity.java
+++ app/src/main/java/com/lgcast/sample/screenmirroring/ScreenMirroringActivity.java
@@ -131,8 +131,6 @@ public class ScreenMirroringActivity extends AppCompatActivity {
         else Toast.makeText(this, getString(R.string.toast_not_started), Toast.LENGTH_SHORT).show();
     }
 
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-
     private boolean hasCapturePermission() {
         Log.v(TAG, "hasCapturePermission");
         return checkSelfPermission(Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED;
