5fdd745cc9d4aa91b0ce69cfc407663e663c01fe
LG Cast Developer <97715188+lgcast-dev@users.noreply.github.com>
2022-05-17 09:49:24 +0900

    Delete AppConfig.java

--- ScreenMirroring-Sampler/ScreenMirroringApp/src/main/java/com/lgcast/sampler/screenmirroring/AppConfig.java
+++
@@ -1,6 +0,0 @@
-package com.lgcast.sampler.screenmirroring;
-
-public class AppConfig {
-    public static final boolean SHOW_DEVICE_INFO = true;
-    public static final boolean USE_ACCESSBILITY_SERVICE_UIBC = false;
-}
